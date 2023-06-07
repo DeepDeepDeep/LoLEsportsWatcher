71aa118e52916641045c895c544b70397a384156
Deep <54153890+DeepDeepDeep@users.noreply.github.com>
2023-06-06 21:45:33 -0500

    Add option for the user to exclude specific leagues from being opened

--- LoLExtension/popup.js
+++ LoLExtension/popup.js
@@ -3,9 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const excludeLeaguesContainer = document.getElementById('excludeLeagues');
 
 	chrome.storage.local.get('windowState', (result) => {
-		if (result.windowState) {
-			select.value = result.windowState;
-		}
+		select.value = result.windowState || 'normal';
 	});
 
 	select.addEventListener('change', () => {
@@ -69,60 +67,56 @@ document.addEventListener('DOMContentLoaded', () => {
 		const excludedLeagues = new Set(result.excludedLeagues || []);
 
 		leagueNames.forEach((leagueName) => {
-			const leagueDiv = document.createElement('div');
-			leagueDiv.classList.add('button-container');
-
-			const leagueLabel = document.createElement('label');
-			leagueLabel.classList.add('label');
-			leagueLabel.textContent = leagueName;
-
-			const leagueButton = document.createElement('button');
-			leagueButton.classList.add('button');
-			leagueButton.dataset.leagueName = leagueName;
-			leagueButton.textContent = excludedLeagues.has(leagueName) ? 'OFF' : 'ON';
-
-			if (excludedLeagues.has(leagueName)) {
-				leagueDiv.classList.add('excluded');
-			}
-
-			leagueButton.addEventListener('click', () => {
-				const isExcluded = excludedLeagues.has(leagueName);
-				if (isExcluded) {
-					leagueButton.textContent = 'ON';
-					leagueDiv.classList.remove('excluded');
-					includeLeague(leagueName);
-					excludedLeagues.delete(leagueName); 
-				} else {
-					leagueButton.textContent = 'OFF';
-					leagueDiv.classList.add('excluded');
-					excludeLeague(leagueName);
-					excludedLeagues.add(leagueName); 
-				}
-				chrome.storage.local.set({ excludedLeagues: Array.from(excludedLeagues) });
-			});
-
-			leagueDiv.appendChild(leagueLabel);
-			leagueDiv.appendChild(leagueButton);
-
+			const leagueDiv = createLeagueDiv(leagueName, excludedLeagues);
 			excludeLeaguesContainer.appendChild(leagueDiv);
 		});
 	});
+
+	function createLeagueDiv(leagueName, excludedLeagues) {
+		const leagueDiv = document.createElement('div');
+		leagueDiv.classList.add('button-container');
+
+		const leagueLabel = document.createElement('label');
+		leagueLabel.classList.add('label');
+		leagueLabel.textContent = leagueName;
+
+		const leagueButton = document.createElement('button');
+		leagueButton.classList.add('button');
+		leagueButton.dataset.leagueName = leagueName;
+		leagueButton.textContent = excludedLeagues.has(leagueName) ? 'OFF' : 'ON';
+
+		leagueButton.addEventListener('click', () => {
+			const isExcluded = excludedLeagues.has(leagueName);
+			toggleLeague(leagueName, excludedLeagues, isExcluded);
+			leagueButton.textContent = isExcluded ? 'ON' : 'OFF';
+			leagueDiv.classList.toggle('excluded');
+			leagueDiv.classList.toggle('included');
+			leagueButton.classList.toggle('off');
+			leagueButton.classList.toggle('on');
+		});
+
+		if (excludedLeagues.has(leagueName)) {
+			leagueDiv.classList.add('excluded');
+			leagueButton.classList.add('off');
+		} else {
+			leagueDiv.classList.add('included');
+			leagueButton.classList.add('on');
+		}
+
+		leagueDiv.appendChild(leagueLabel);
+		leagueDiv.appendChild(leagueButton);
+
+		return leagueDiv;
+	}
+
+	function toggleLeague(leagueName, excludedLeagues, isExcluded) {
+		if (isExcluded) {
+			excludedLeagues.delete(leagueName);
+			console.log('Including league:', leagueName);
+		} else {
+			excludedLeagues.add(leagueName);
+			console.log('Excluding league:', leagueName);
+		}
+		chrome.storage.local.set({ excludedLeagues: Array.from(excludedLeagues) });
+	}
 });
-
-function excludeLeague(leagueName) {
-	chrome.storage.local.get('excludedLeagues', (result) => {
-		const excludedLeagues = new Set(result.excludedLeagues || []);
-		excludedLeagues.add(leagueName);
-		chrome.storage.local.set({ excludedLeagues: Array.from(excludedLeagues) });
-		console.log('Excluding league:', leagueName);
-	});
-}
-
-function includeLeague(leagueName) {
-	chrome.storage.local.get('excludedLeagues', (result) => {
-		const excludedLeagues = new Set(result.excludedLeagues || []);
-		excludedLeagues.delete(leagueName);
-		chrome.storage.local.set({ excludedLeagues: Array.from(excludedLeagues) });
-		console.log('Including league:', leagueName);
-	});
-}
